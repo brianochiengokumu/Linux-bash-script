32a05e2e9e91f47aa083420133ce8df680db1acc
brianochiengokumu <brianochiengokumu@gmail.com>
2020-11-24 14:39:29 +0300

    adding the git pull

--- git.sh
+++ git.sh
@@ -2,9 +2,10 @@
 #this is a shellscript to automatically git add push and commit in one go
 
 function mygit() {
-	git add .
-	git commit -am "$1" # this is to allow for external input of your commit comment 
-	git push 
+    git pull --all
+    git add .
+    git commit -am "$1" # this is to allow for external input of your commit comment
+    git push
 }
 
 
